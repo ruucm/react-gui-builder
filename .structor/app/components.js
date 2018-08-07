34017d4b00d7f53133bd1fe491438179344bebee
ruucm <ruucm.a@gmail.com>
2018-08-08 07:52:48 +0900

    add CSSPatens module

--- .structor/app/components.js
+++ .structor/app/components.js
@@ -1,6 +1,5 @@
-import * as Centering from 'modules/Centering'
 import * as Animations from 'modules/Animations'
 import * as Router from 'modules/Router'
 import * as Layout from 'modules/Layout'
-
-export { Centering, Animations, Router, Layout }
+import * as CSSPaterns from 'modules/CSSPaterns'
+export { Animations, Router, Layout, CSSPaterns }
--- app/modules/Centering/index.js
+++
@@ -1,3 +0,0 @@
-import Center from './components/Center'
-
-export { Center }
